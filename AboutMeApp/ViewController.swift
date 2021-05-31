5e470d05d5cb61bd2531ca7e80bab5123b8fbaf7
Egor Gorskikh <egorgorskikh@gmail.com>
2021-05-31 14:10:20 +0300

    Initial Commit

---
+++ AboutMeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeApp
+//
+//  Created by Egor Gorskikh on 31.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
